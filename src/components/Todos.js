5407a8676c88f6d84c183128005290cc0030fd34
Steve Dukes <sdukes@gmail.com>
2018-12-27 10:48:54 -0500

    clean up, todos comments

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,6 +3,12 @@ import Delete from '@material-ui/icons/Delete';
 import Done from '@material-ui/icons/Done';
 import Undo from '@material-ui/icons/Undo';
 
+/**
+ * This is a pure function that returns jsx for the list of todos
+ * @param {*} todos array all the todos to be listed
+ * @param handleToggle func toggless the todo state from completed to not completed
+ * @param handleDelete func deletes a todo from the list
+ */
 const Todos = ({
   todos,
   handleToggle,
--- src/containers/TodosList.js
+++ src/containers/TodosList.js
@@ -3,9 +3,6 @@ import { connect } from 'react-redux';
 import { compose } from 'ramda';
 import { REMOVE_TODO, TOGGLE_TODO } from '../utils/constants';
 import Todos from '../components/Todos';
-import Delete from '@material-ui/icons/Delete';
-import Done from '@material-ui/icons/Done';
-import Undo from '@material-ui/icons/Undo';
 import '../App.css';
 
 class TodosList extends Component {
@@ -20,16 +17,6 @@ class TodosList extends Component {
     return (
       <Fragment>
         <p>{`${numTodos} todo item${numTodos !== 1 ? 's' : ''}`}</p>
-        <div>
-          {/* {this.props.todos.map(todo => (
-            <div key={todo.id.toString()} className="todo-row">
-              <div className={`todos ${todo.completed ? 'done' : ''}`}>{todo.text}</div>
-              <div>{!todo.completed && <button onClick={() => this.handleToggle(todo.id)}><Done /></button>}</div>
-              <div>{todo.completed && <button onClick={() => this.handleToggle(todo.id)}><Undo /></button>}</div>
-              <button onClick={() => this.handleDelete(todo.id)}><Delete /></button>
-            </div>
-          ))} */}
-        </div>
         <Todos todos={this.props.todos} handleDelete={this.handleDelete} handleToggle={this.handleToggle} />
       </Fragment>
     )
